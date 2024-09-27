8af9f0e179e321bde2d01af987ec1e5b9f0c8375
PedroHSdeAlmeida <106109294+PedroHSdeAlmeida@users.noreply.github.com>
2024-09-27 09:46:03 -0300

    feat: Integração com o serviço de IA

--- src/controllers/imagemController.ts
+++ src/controllers/imagemController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { obterImagens } from "../services";
+import { obterImagens, processarImagens } from "../services";
 
 export const buscarImagens = async (req: Request, res: Response): Promise<void> => {
   const { bbox, datetime } = req.query;
@@ -11,8 +11,9 @@ export const buscarImagens = async (req: Request, res: Response): Promise<void> 
 
   try {
     const imagens = await obterImagens(bbox as string, datetime as string);
-    res.status(200).json(imagens);
+    const imagensProcessadas = await processarImagens(imagens);
+    res.status(200).json(imagensProcessadas);
   } catch (erro) {
-    res.status(500).json({ erro: "Erro ao buscar as imagens." });
+    res.status(500).json({ erro: "Erro ao buscar e processar as imagens." });
   }
 };
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,1 +1,2 @@
 export * from './imagemService';
+export * from './iaService';
